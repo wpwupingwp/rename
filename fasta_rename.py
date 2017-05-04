019964da5ed3f598f2e4cbd265fae9fe3c51c8ce
Workstation <wpwupingwp@outlook.com>
2017-05-04 09:56:54 +0800

    finish rename()

--- fasta_rename.py
+++ fasta_rename.py
@@ -4,7 +4,6 @@ from glob import glob
 from os import mkdir
 from os.path import join as join_path
 from timeit import default_timer as timer
-import re
 # from sys import argv
 
 
@@ -16,24 +15,38 @@ def get_format():
     return new_format
 
 
-def rename(file_list, new_format):
-    pattern = new_format
+def rename(file_list, new_format, out):
     for fasta in file_list:
-        with open(fasta, 'r') as old:
-            re.split()
+        with open(fasta, 'r') as old, open(
+                join_path(out, fasta+'.rename'), 'w') as new:
+            for line in old:
+                # skip sequence
+                if line[0] != '>':
+                    new.write(line)
+                    continue
+                else:
+                    pick = list()
+                    fields = line[1:].split('|')
+                    for n in new_format:
+                        pick.append(fields[int(n)-1].strip())
+                    new.write('>{}\n'.format('|'.join(pick)))
 # to be continue
 
 
 def main():
-    start = timer()
     # gb = argv[1]
     file_list = glob('*.fasta')
     OUT = 'renamed'
     mkdir(OUT)
 
     new_format = get_format()
-    rename(file_list, new_format)
+    start = timer()
+    rename(file_list, new_format, OUT)
 
     end = timer()
     print('''\nFinished with {0:.3f} s. You can find fasta file in the folder
     {1}.'''.format(end-start, OUT))
+
+
+if __name__ == '__main__':
+    main()
