be6ef1e8b102e08b561f44c44cfa9885e17652e1
Workstation <wpwupingwp@outlook.com>
2017-06-15 17:51:25 +0800

    rewrite fasta_divide to use any sep and not to use glob

--- fasta_divide.py
+++ fasta_divide.py
@@ -1,43 +1,58 @@
 #!/usr/bin/python3
 
-from glob import glob
-from os import mkdir
-from os.path import join as join_path
 from timeit import default_timer as timer
 from Bio import SeqIO
-# from sys import argv
+import argparse
+import os
+import re
 
 
-def get_format():
-    OLD = '1.gene|2.order|3.family|4.genus|5.species|6.accession_id|7.specimen'
-    print(OLD)
-    choice = input('Input number of field you want to use to divide:\n')
+def get_choice(fasta, sep):
+    # read line until get id
+    with open(fasta, 'r') as raw:
+        for line in raw:
+            if line.startswith('>'):
+                break
+    print('This is raw id of one of sequence you give:')
+    print(line, '\n')
+    splitted = re.split(sep, line[1:])
+    for index, match in enumerate(splitted, 1):
+        print('{}.{}'.format(index, match))
+        index += 1
+    choice = input(
+        'Choose seperator you want by input number of the field')
     return int(choice)
 
 
-def divide(file_list, choice, out):
-    for fasta in file_list:
-        for record in SeqIO.parse(fasta, 'fasta'):
-            item = record.id.split('|')[choice-1]
-            # remove illegal characters
-            item = item.replace('/', '_')
-            handle = open(join_path(out, item+'.fasta'), 'a')
-            SeqIO.write(record, handle, 'fasta')
+def divide(fasta, sep, choice, out):
+    for record in SeqIO.parse(fasta, 'fasta'):
+        item = re.split(sep, record.id)
+        # remove illegal characters
+        item = item[choice-1]
+        with open(os.path.join(out, item+'.fasta'), 'a') as output:
+            SeqIO.write(record, output, 'fasta')
 
 
 def main():
-    # gb = argv[1]
-    file_list = glob('*.fasta')
-    OUT = 'divide'
-    mkdir(OUT)
-
-    new_format = get_format()
     start = timer()
-    divide(file_list, new_format, OUT)
+    args = argparse.ArgumentParser(description=main.__doc__)
+    args.add_argument('input', help='input file as fasta format')
+    args.add_argument('-s', type=int, dest='choice',
+                      help='the field you want to use')
+    args.add_argument('-o', '--output', default='out',
+                            help='output directory')
+    args = args.parse_args()
+
+    SEP = re.compile(r'[\-\|/\\:;~!\?@#$%^&\*+=]')
+    os.mkdir(args.out)
+    if args.choice is None:
+        choice = get_choice(args.input, SEP)
+    start = timer()
+    divide(args.input, SEP, choice, args.out)
 
     end = timer()
     print('''\nFinished with {0:.3f} s. You can find fasta file in the folder
-    {1}.'''.format(end-start, OUT))
+    {1}.'''.format(end-start, args.out))
 
 
 if __name__ == '__main__':
