b00c3405af045eff47048a30783de6717993cec8
Workstation <wpwupingwp@outlook.com>
2017-06-29 18:08:17 +0800

    split argpase part out of main

--- large_query.py
+++ large_query.py
@@ -11,16 +11,21 @@ print(query)
 handle = Entrez.read(Entrez.esearch(db='nuccore', term=query,
                                     usehistory='y',))
 count = handle['Count']
+count = int(count)
+print(count)
 Retstart = 1
 output_file = open('sequence.gb', 'w')
-while True:
+while Retstart <= count:
     print(Retstart)
-    genome_content = Entrez.efetch(db='nuccore',
-                                   webenv=handle['WebEnv'],
-                                   query_key=handle['QueryKey'],
-                                   rettype='gb',
-                                   retmode='text',
-                                   retstart=Retstart,
-                                   Retmax=100000)
-    output_file.write(genome_content.read())
-    Retstart = Retstart + 100000
+    try:
+        genome_content = Entrez.efetch(db='nuccore',
+                                       webenv=handle['WebEnv'],
+                                       query_key=handle['QueryKey'],
+                                       rettype='gb',
+                                       retmode='text',
+                                       retstart=Retstart,
+                                       retmax=2000)
+        output_file.write(genome_content.read())
+    except:
+        continue
+    Retstart = Retstart + 2000
--- large_query_with_id_list.py
+++ large_query_with_id_list.py
@@ -1,26 +1,57 @@
 #!/usr/bin/python3
 
 from Bio import Entrez
+from timeit import default_timer as timer
+import argparse
 
 
-EMAIL = 'wpwupingwp@outlook.com'
-Entrez.email = EMAIL
-# set 'noexp' to fetch only this level
-query = input('Query string:\n')
-print(query)
-handle = Entrez.read(Entrez.esearch(db='nuccore', term=query,
-                                    usehistory='y',))
-count = handle['Count']
-Retstart = 1
-output_file = open('sequence.gb', 'w')
-while True:
-    print(Retstart)
-    genome_content = Entrez.efetch(db='nuccore',
-                                   webenv=handle['WebEnv'],
-                                   query_key=handle['QueryKey'],
-                                   rettype='gb',
-                                   retmode='text',
-                                   retstart=Retstart,
-                                   Retmax=100000)
-    output_file.write(genome_content.read())
-    Retstart = Retstart + 100000
+def get_id_list(id_list_file):
+    N = 1000
+    with open(id_list_file) as raw:
+        to_down = list()
+        for index, line in enumerate(raw):
+            to_down.append(line.strip())
+            if index % N == 0:
+                yield to_down
+                to_down = list()
+
+
+def down(id_list_file, email, output):
+    Entrez.email = email
+    output_file = open(output, 'w')
+    for to_down in get_id_list(id_list_file):
+        id_list = ', '.join(to_down)
+        print('id list', id_list)
+        handle = Entrez.read(Entrez.esearch(db='nuccore',
+                                          term=id_list,
+                                          usehistory='y',))
+        print('handle', handle)
+        genome_content = Entrez.efetch(db='nuccore',
+                                       webenv=handle['WebEnv'],
+                                       query_key=handle['QueryKey'],
+                                       rettype='gb',
+                                       retmode='text')
+        output_file.write(genome_content.read())
+
+
+def parse_args():
+    arg = argparse.ArgumentParser()
+    arg.add_argument('id_list', help='Accession ID list downloaded from NCBI')
+    arg.add_argument('-email', default='wpwupingwp@outlook.com',
+                     help='email address')
+    arg.add_argument('-out', default='sequence.gb', help='output filename')
+    return arg.parse_args()
+
+
+def main():
+    start = timer()
+    arg = parse_args()
+    print(arg)
+
+    down(arg.id_list, arg.email, arg.out)
+    end = timer()
+    print('Cost {:.3f} seconds.'.format(end-start))
+
+
+if __name__ == '__main__':
+    main()
