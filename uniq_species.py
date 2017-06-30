5c651899c037bbd5ec3a7e245d2d24b5b1761acc
Workstation <wpwupingwp@outlook.com>
2017-06-30 16:45:37 +0800

    fix handle

--- uniq_species.py
+++ uniq_species.py
@@ -9,7 +9,6 @@ def main():
     start = timer()
     raw = SeqIO.parse(argv[1], 'fasta')
     name_seq = dict()
-    output = list()
     before = 0
     after = 0
     for record in raw:
@@ -33,20 +32,23 @@ def main():
 
     print('Duplicated sequences:')
     log = open(argv[1]+'.log', 'w')
+    output = open(argv[1]+'.uniq', 'w')
+    log.write('Before\tAfter\n')
     for record in name_seq.values():
         after += 1
         longest = get_longest(record)
+        SeqIO.write(longest, output, 'fasta')
         if len(record) != 1:
             id_list = [i.id for i in record]
             log.write('Longest:\t{} in ({})\n'.format(
                 longest.id, '\t'.join(id_list)))
-    log.write('Before\t{}\tAfter\t{}\n'.format(before, after))
-    with open(argv[1]+'.uniq', 'a') as output_file:
-        SeqIO.write(output, output_file, 'fasta')
+    log.write('{}\t\t{}\n'.format(before, after))
     end = timer()
     print('Total {} sequences in {} format.'.format(before, 'fasta'))
     print('{} sequences left in the file {}.uniq.'.format(after, argv[1]))
     print('Cost {:.3f} seconds.'.format(end-start))
+    log.close()
+    output.close()
 
 
 if __name__ == '__main__':
