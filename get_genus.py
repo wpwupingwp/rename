4ad9d44a34d3dab8c5d6e11304767eb714e5c1f3
Workstation <wpwupingwp@outlook.com>
2018-08-01 18:46:30 +0800

    add get_genus

---
+++ get_genus.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import json
+from sys import argv
+
+with open('./genus_dict.json', 'r') as _:
+    name_dict = json.load(_)
+
+old = open(argv[1], 'r')
+new = open(argv[1]+'.rename', 'w')
+for line in old:
+    if line[0] == '>':
+        (gene, kingdom, order, family, genus, species, accession,
+         *_) = line.strip().split(',')
+        species_clean = species.split('_')[0]
+        if family == '':
+            kingdom, order, family = name_dict[genus+'|'+species_clean]
+            new.write('|'.join([gene, kingdom, order, family, genus, species,
+                                accession, *_])+'\n')
+    else:
+        new.write(line)
