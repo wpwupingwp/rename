518da46e58fcafdbd8c8b937c67db8469ce34547
Workstation <wpwupingwp@outlook.com>
2020-03-10 18:47:22 +0800

    fix rename_for_windows

--- rename_for_windows.py
+++ rename_for_windows.py
@@ -2,14 +2,18 @@
 
 from pathlib import Path
 
+d_suffix = '.delete_because_duplicate'
 files = list(Path('.').glob('*'))
 files_lower = dict()
 for f in files:
+    if f.suffix == d_suffix:
+        continue
     lower = str(f).lower()
-    if lower in files:
+    if lower in files_lower:
         print(f, files_lower[lower])
         # pathlib do not support write append
         with open(files_lower[lower], 'a') as out:
             out.write(f.read_text())
-        f.rename(f.with_suffix('.delete'))
-    files_lower[lower] = f
+        f.rename(f.with_suffix(d_suffix))
+    else:
+        files_lower[lower] = f
