7cd4bae8be870b85edba87e45c563480833beb5d
Workstation <wpwupingwp@outlook.com>
2021-07-21 16:32:02 +0800

    update uniq

--- uniq.py
+++ uniq.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
 
 from Bio import SeqIO
-from timeit import default_timer as timer
 import argparse
 import re
 
 
-def parse_args():
-    args = argparse.ArgumentParser(description=main.__doc__)
-    args.add_argument('input', help='input file as fasta format')
-    args.add_argument('-c', type=str, dest='choice',
-                      help='the field you want to use')
-    args.add_argument('-m', '--method', default='longest',
-                      help='method to get the only sequence')
-    args.print_help()
-    return args.parse_args()
+def parse_arg():
+    arg = argparse.ArgumentParser(description=main.__doc__)
+    arg.add_argument('input', help='input file as fasta format')
+    arg.add_argument('-c', type=str, dest='choice',
+                     help='the field you want to use')
+    arg.add_argument('-m', '--method', default='longest',
+                     help='method to get the only sequence')
+    arg.print_help()
+    return arg.parse_args()
 
 
 def get_choice(fasta, sep):
@@ -34,7 +33,7 @@ def get_choice(fasta, sep):
     return choice
 
 
-def uniq(args, sep):
+def uniq(arg, sep):
     def get_longest(records):
         info = list()
         for record in records:
@@ -44,10 +43,10 @@ def uniq(args, sep):
         longest = max(info, key=lambda x: x[0])
         return longest[1]
 
-    if args.choice is None:
-        args.choice = get_choice(args.input, sep)
-    choice = [int(i)-1 for i in args.choice.split(' ')]
-    raw = SeqIO.parse(args.input, 'fasta')
+    if arg.choice is None:
+        arg.choice = get_choice(arg.input, sep)
+    choice = [int(i)-1 for i in arg.choice.split(' ')]
+    raw = SeqIO.parse(arg.input, 'fasta')
     name_seq = dict()
     before = 0
     after = 0
@@ -64,9 +63,8 @@ def uniq(args, sep):
             name_seq[name] = [record, ]
         before += 1
 
-    print('Duplicated sequences:')
-    log = open(args.input+'.log', 'w')
-    output = open(args.input+'.uniq', 'w')
+    log = open(arg.input+'.log', 'w')
+    output = open(arg.input+'.uniq', 'w')
     for record in name_seq.values():
         after += 1
         longest = get_longest(record)
@@ -77,20 +75,15 @@ def uniq(args, sep):
                 longest.id, '\t'.join(id_list)))
     log.write('Before\tAfter\n')
     log.write('{}\t\t{}\n'.format(before, after))
-    print('Total {} sequences in {} format.'.format(before, 'fasta'))
-    print('{} sequences left in the file {}.uniq.'.format(after, args.input))
+    print(f'{arg.input}, before {before}, after {after}')
     log.close()
     output.close()
 
 
 def main():
-    start = timer()
-    args = parse_args()
-
+    arg = parse_arg()
     SEP = re.compile(r'[\|/\\:;~!\?@#$%^&\*+=]')
-    uniq(args, SEP)
-    end = timer()
-    print('Cost {:.3f} seconds.'.format(end-start))
+    uniq(arg, SEP)
 
 
 if __name__ == '__main__':
